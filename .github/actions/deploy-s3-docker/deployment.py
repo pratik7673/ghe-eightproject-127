7e5b1782f8dca133ff3b00188780659138d11de8
Jena Pratik (FCA) <t1553pj@cag.cdroot.net>
2025-03-26 23:35:37 +0530

    done python3.12

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -2,31 +2,38 @@ import os
 import boto3
 import mimetypes
 from botocore.config import Config
-
+from botocore.exceptions import NoCredentialsError
 
 def run():
-    bucket = os.environ['INPUT_BUCKET']
-    bucket_region = os.environ['INPUT_BUCKET-REGION']
-    dist_folder = os.environ['INPUT_DIST-FOLDER']
+    try:
+        bucket = os.environ['INPUT_BUCKET']
+        bucket_region = os.environ['INPUT_BUCKET-REGION']
+        dist_folder = os.environ['INPUT_DIST-FOLDER']
 
-    configuration = Config(region_name=bucket_region)
+        # Ensure AWS credentials are set
+        if not os.getenv("AWS_ACCESS_KEY_ID") or not os.getenv("AWS_SECRET_ACCESS_KEY"):
+            raise NoCredentialsError
 
-    s3_client = boto3.client('s3', config=configuration)
+        configuration = Config(region_name=bucket_region)
+        s3_client = boto3.client('s3', config=configuration)
 
-    for root, subdirs, files in os.walk(dist_folder):
-        for file in files:
-            s3_client.upload_file(
-                os.path.join(root, file),
-                bucket,
-                os.path.join(root, file).replace(dist_folder + '/', ''),
-                ExtraArgs={"ContentType": mimetypes.guess_type(file)[0]}
-            )
+        for root, subdirs, files in os.walk(dist_folder):
+            for file in files:
+                s3_client.upload_file(
+                    os.path.join(root, file),
+                    bucket,
+                    os.path.join(root, file).replace(dist_folder + '/', ''),
+                    ExtraArgs={"ContentType": mimetypes.guess_type(file)[0]}
+                )
 
-    website_url = f'http://{bucket}.s3-website-{bucket_region}.amazonaws.com'
-    # The below code sets the 'website-url' output (the old ::set-output syntax isn't supported anymore - that's the only thing that changed though)
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
-        print(f'website-url={website_url}', file=gh_output)
+        website_url = f'http://{bucket}.s3-website-{bucket_region}.amazonaws.com'
+        
+        # Save output for GitHub Actions
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
+            print(f'website-url={website_url}', file=gh_output)
 
+    except NoCredentialsError:
+        print("Error: AWS credentials not found. Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.")
 
 if __name__ == '__main__':
     run()
